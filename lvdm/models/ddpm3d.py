f171ff7449803603dc1cc49df23979f9248b36aa
dailingx <468551414@qq.com>
2024-03-16 22:41:38 +0800

    init

--- lvdm/models/ddpm3d.py
+++ lvdm/models/ddpm3d.py
@@ -719,16 +719,18 @@ class LatentVisualDiffusion(LatentDiffusion):
     def configure_optimizers(self):
         """ configure_optimizers for LatentDiffusion """
         lr = self.learning_rate
-        if self.empty_params_only and hasattr(self, "empty_paras"):
-            params = [p for n, p in self.model.named_parameters() if n in self.empty_paras]
-            print('self.empty_paras', len(self.empty_paras))
-            for n, p in self.model.named_parameters():
-                if n not in self.empty_paras:
-                    p.requires_grad = False
-            mainlogger.info(f"@Training [{len(params)}] Empty Paramters ONLY.")
-        else:
-            params = list(self.model.parameters())
-            mainlogger.info(f"@Training [{len(params)}] Full Paramters.")
+        # if self.empty_params_only and hasattr(self, "empty_paras"):
+        #     params = [p for n, p in self.model.named_parameters() if n in self.empty_paras]
+        #     print('self.empty_paras', len(self.empty_paras))
+        #     for n, p in self.model.named_parameters():
+        #         if n not in self.empty_paras:
+        #             p.requires_grad = False
+        #     mainlogger.info(f"@Training [{len(params)}] Empty Paramters ONLY.")
+        # else:
+        #     params = list(self.model.parameters())
+        #     mainlogger.info(f"@Training [{len(params)}] Full Paramters.")
+        params = list(self.model.parameters())
+        mainlogger.info(f"@Training [{len(params)}] Full Paramters.")
 
         if self.learn_logvar:
             mainlogger.info('Diffusion model optimizing logvar')
