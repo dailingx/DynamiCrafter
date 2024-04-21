9fb272917edb439bedecc27b186028a078052760
dailingx <468551414@qq.com>
2024-04-21 16:14:52 +0800

    add test

--- lvdm/models/ddpm3d.py
+++ lvdm/models/ddpm3d.py
@@ -704,6 +704,8 @@ class LatentDiffusion(DDPM):
             target = x_start
         elif self.parameterization == "eps":
             target = noise
+        elif self.parameterization == "v":
+            target = self.get_v(x_start, noise, t)
         else:
             raise NotImplementedError()
 
@@ -743,6 +745,37 @@ class LatentDiffusion(DDPM):
         loss_dict.update({f'{prefix}/loss': loss})
 
         return loss, loss_dict
+
+    # def p_losses(self, x_start, t, noise=None):
+    #     noise = default(noise, lambda: torch.randn_like(x_start))
+    #     x_noisy = self.q_sample(x_start=x_start, t=t, noise=noise)
+    #     model_out = self.model(x_noisy, t)
+    #
+    #     loss_dict = {}
+    #     if self.parameterization == "eps":
+    #         target = noise
+    #     elif self.parameterization == "x0":
+    #         target = x_start
+    #     elif self.parameterization == "v":
+    #         target = self.get_v(x_start, noise, t)
+    #     else:
+    #         raise NotImplementedError(f"Parameterization {self.parameterization} not yet supported")
+    #
+    #     loss = self.get_loss(model_out, target, mean=False).mean(dim=[1, 2, 3])
+    #
+    #     log_prefix = 'train' if self.training else 'val'
+    #
+    #     loss_dict.update({f'{log_prefix}/loss_simple': loss.mean()})
+    #     loss_simple = loss.mean() * self.l_simple_weight
+    #
+    #     loss_vlb = (self.lvlb_weights[t] * loss).mean()
+    #     loss_dict.update({f'{log_prefix}/loss_vlb': loss_vlb})
+    #
+    #     loss = loss_simple + self.original_elbo_weight * loss_vlb
+    #
+    #     loss_dict.update({f'{log_prefix}/loss': loss})
+    #
+    #     return loss, loss_dict
 
 
 class LatentVisualDiffusion(LatentDiffusion):
